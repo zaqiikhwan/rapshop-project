25b1790616b2fea55b9d613e7de0cc833d8f0a3d
Muh Zaqi <zaqiikhwan22@gmail.com>
2023-03-10 20:37:22 +0700

    refactor: len(data pembelian)

--- model/model.stock_dl.go
+++ model/model.stock_dl.go
@@ -8,7 +8,6 @@ type InputStockDL struct {
 	Profit  int `json:"profit"`
 	HargaJualDL int `json:"harga_jual_dl"`
 	HargaBeliDL int `json:"harga_beli_dl"`
-	HargaJualBGL int `json:"harga_jual_bgl"`
 	HargaBeliBGL int `json:"harga_beli_bgl"`
 }
 
