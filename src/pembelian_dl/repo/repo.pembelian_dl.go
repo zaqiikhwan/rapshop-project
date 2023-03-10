606d19921017eec819b2e8c37737bc0acdc6c0b4
Muh Zaqi <zaqiikhwan22@gmail.com>
2023-03-10 14:19:23 +0700

    get all pembayaran with success transaction

--- src/pembelian_dl/repo/repo.pembelian_dl.go
+++ src/pembelian_dl/repo/repo.pembelian_dl.go
@@ -25,7 +25,7 @@ func(rp *repoPembelianDL) Create(input entities.PembelianDL) error {
 func(rp *repoPembelianDL) GetAll(_startInt int, _endInt int) ([]entities.PembelianDL, int, error) {
 	var allPembelian []entities.PembelianDL
 	var lenData []entities.PembelianDL
-	if err := rp.db.Order("created_at desc").Offset(_startInt - 1).Limit(_endInt - _startInt + 1).Find(&allPembelian).Error; err != nil {
+	if err := rp.db.Order("created_at desc").Where("status_pembayaran = ?", "success").Offset(_startInt - 1).Limit(_endInt - _startInt + 1).Find(&allPembelian).Error; err != nil {
 		return allPembelian, 0, err
 	}
 	if err := rp.db.Find(&lenData).Error; err != nil {
